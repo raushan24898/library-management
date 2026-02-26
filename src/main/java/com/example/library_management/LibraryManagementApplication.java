fcc1499599891cf3d9b2b74dc6ca94093206e5d2
raushanraj <raushan.raj@sakhaglobal.com>
2026-02-26 11:58:39 +0530

    Trigger pipeline

--- src/main/java/com/example/library_management/LibraryManagementApplication.java
+++ src/main/java/com/example/library_management/LibraryManagementApplication.java
@@ -9,5 +9,6 @@ public class LibraryManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApplication.class, args);
 	}
+	// test the CI/CD pipeline
 
 }
